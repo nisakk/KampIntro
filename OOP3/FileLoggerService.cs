3a49b7887f61014498f9bd1e60f46894aea9925f
nisa küçükkömürcü <nisakucukkomurcu@gmail.com>
2021-01-31 19:31:04 +0300

    Ekleme Yapıldı

---
+++ OOP3/FileLoggerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+	class FileLoggerService: ILoggerService
+	{
+		public void Log()
+
+		{
+			Console.WriteLine("Dosyaya  Loglandı");
+
+		}
+
+	}
+}	
